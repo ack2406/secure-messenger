5c217f7c7a790fb23ce49513b94c70bdbce535ec
ack2406 <ack9222@gmail.com>
2023-05-27 23:51:43 +0200

    feat: add Chakra styling

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,4 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ChakraProvider } from '@chakra-ui/react'
+import { io } from 'socket.io-client'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+)
